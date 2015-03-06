6b575624ac3e6e011e82e3a07db0d10ea2748d83
Chris Slowik <chris@creativeda.sh>
2015-03-05 23:14:57 -0800

    added watchkit app

---
+++ LocalVocal WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  LocalVocal WatchKit Extension
+//
+//  Created by Chris Slowik on 3/5/15.
+//  Copyright (c) 2015 Chris Slowik. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ LocalVocal WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  LocalVocal WatchKit Extension
+//
+//  Created by Chris Slowik on 3/5/15.
+//  Copyright (c) 2015 Chris Slowik. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
